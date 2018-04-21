96fdeb12e30cf5b696d8ec1a02f98eba4300803d
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-21 19:51:47 +0700

    arrow function

---
+++ a.js
@@ -0,0 +1,12 @@
+// const a = console.log;
+// a(1);
+
+function add(a, b) {
+    return a + b;
+}
+
+// const add2 = add;
+
+// console.log(add(1, 2));
+
+console.log(console.log());
---
+++ c.js
@@ -0,0 +1,13 @@
+function doSth(fn) {
+    return fn(1);
+}
+
+doSth(console.log);
+
+function binhPhuong(x) {
+    return x * x;
+}
+
+const b2 = x => x * x;
+
+console.log(b2(10));
